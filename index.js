066e3c51ffcb887ddbf70836d8d9c3db8686d8e0
wyjeon <wyjeon.info@gmail.com>
2021-03-16 13:41:22 +0900

    MongoDB 연결

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
+
+const config = require('./config/key');
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('hello!'));
 
