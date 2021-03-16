da5d790576209e7bcf5102cadde3bafb8637e491
wyjeon <wyjeon.info@gmail.com>
2021-03-16 14:14:43 +0900

    body-parser & 회원 가입 기능

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const port = 5000;
 
 const config = require('./config/key');
 
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose');
 mongoose
   .connect(config.mongoURI, {
@@ -16,5 +24,16 @@ mongoose
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('hello!'));
+
+app.post('/register', (req, res) => {
+  // 회원 가입에 필요한 정보를 client에서 가져와서 DB에 넣어준다.
+  const user = new User(req.body);
+
+  // mongoDB에 저장
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
