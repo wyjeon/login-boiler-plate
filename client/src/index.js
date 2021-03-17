ce352664e16aab86fe4b701015f713218b8903fc
wyjeon <wyjeon.info@gmail.com>
2021-03-17 12:42:09 +0900

    redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.css';
 
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStoreWithMiddleware(Reducer, composeWithDevTools())}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
