d093bb86c612ec246c3dabe5b35700dec47d469e
wyjeon <wyjeon.info@gmail.com>
2021-03-17 14:01:46 +0900

    로그아웃

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get('/api/hello').then(response => console.log(response));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then(response => {
+      if (response.data.success) {
+        props.history.push('/login');
+      } else {
+        alert('로그아웃 하는데 실패했습니다.');
+      }
+    });
+  };
 
   return (
     <div
@@ -17,6 +27,7 @@ function LandingPage() {
       }}
     >
       LandingPage
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
