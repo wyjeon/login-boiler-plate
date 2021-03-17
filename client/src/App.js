10253e38f9cc38574cc429a4c3e054bbfa24035c
wyjeon <wyjeon.info@gmail.com>
2021-03-17 14:30:43 +0900

    인증 체크

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Auth from './hoc/auth';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/register" component={RegisterPage} />
+        <Route exact path="/" component={Auth(LandingPage, null)} />
+        <Route exact path="/login" component={Auth(LoginPage, false)} />
+        <Route exact path="/register" component={Auth(RegisterPage, false)} />
       </Switch>
     </Router>
   );
